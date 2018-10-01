4b0abac2d94e0683847f59da738eb8b2edfe4e25
gkeswani92 <gauravkeswani92@gmail.com>
2018-10-01 00:35:45 -0700

    Adding react to the project, creating the first component and having it rendered via webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<p>Hello World from React!</p>);
+    }
+}
+ 
+render(<App />, document.getElementById('root'));
