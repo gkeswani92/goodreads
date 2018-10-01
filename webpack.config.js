82184a63ac239c5b4bad82b62dda48b9747a52dd
gkeswani92 <gauravkeswani92@gmail.com>
2018-09-30 22:23:08 -0700

    Editing the web pack config and making it a dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-    entry: './src/app.js',
+var webpack = require('webpack');
+var path = require('path');
+
+// Path where the bundle file and other webpack output should be placed
+var BUILD_DIR = path.join(__dirname, 'dist');
+
+// Path where the source code will be found
+var APP_DIR = path.join(__dirname, 'src');
+
+var config = {
+    entry: APP_DIR + '/app.js',
     output: {
-        filename: './dist/app.bundle.js',
+        path: BUILD_DIR,
+        filename: 'app.bundle.js',
     }
 }
+
+module.exports = config;
